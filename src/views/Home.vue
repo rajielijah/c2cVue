b02baa47bba8bd58368f3bf256eb487caac4826e
Raji Elijah <rajielijah@gmail.com>
2021-04-08 15:10:02 +0100

    first name

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Can this work?</h2>
+  </div>
+
+  
+</template>
+
+<script>
+
+</script>
